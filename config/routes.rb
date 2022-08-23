43f73e75d46df2afd98e1dc678c3e5f15040199a
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-08-23 10:35:28 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'registrations/new'
-  get 'registrations/create'
+  resources :registrations, only:  %i[new create]
+  resources :sessions, only:  %i[new create]
   resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
