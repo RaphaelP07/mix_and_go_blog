604d50d1d56d75c7636225fa37d7903646a9bb18
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-08-23 10:32:50 +0800

    generated controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'registrations/new'
+  get 'registrations/create'
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
